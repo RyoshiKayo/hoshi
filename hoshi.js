6b611711804ed22fa70662357e924efba6ad38c4
Kayo <RyoshiKayo@users.noreply.github.com>
2019-07-27 21:42:59 -0700

    Update hoshi.js

--- hoshi.js
+++ hoshi.js
@@ -1,10 +1,21 @@
 require('./src/util/Extensions');
 
-const config = require('./config.json');
 const HoshiClient = require('./src/struct/HoshiClient');
 const Logger = require('./src/util/Logger');
 const Sentry = require('@sentry/node');
-
+let config;
+try {
+	config = require('./config.json');
+} catch (err) {
+	Logger.warn('./config.json doesn\'t exist, trying environment variables...');
+	config = {
+		"token": process.env.HOSHI_TOKEN,
+		"owner": process.env.HOSHI_OWNER_IDS.split(','), //"123992700587343872,86890631690977280"
+		"dbURL": process.env.HOSHI_DB_URL,
+		"sentryKey": process.env.HOSHI_SENTRY_KEY,
+		"selfstarWarningTimeout": (process.env.HOSHI_SELF_STAR_WARNING_TIMEOUT === undefined) ? 60000 : process.env.HOSHI_SELF_STAR_WARNING_TIMEOUT
+	}
+}
 const client = new HoshiClient(config);
 
 if (config.sentryKey) {
